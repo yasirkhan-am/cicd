4e0f61e93c4ccdb8758ed7c4cb1aa25590ae4627
yasirkhan-am <muhammad.yasir@artisticmilliners.com>
2026-07-01 14:59:14 +0500

    frist commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+PORT = int(os.getenv("PORT", 8000))
+
+@app.route("/")
+def home():
+    return "Hello Python App"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
