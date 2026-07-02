4f70269809f7eba52d9561c76b68150a4703b162
yasirkhan-am <muhammad.yasir@artisticmilliners.com>
2026-07-02 15:56:44 +0500

    working fine

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ PORT = int(os.getenv("PORT", 8000))
 
 @app.route("/")
 def home():
-    return "Hello python python app"
+    return "Hello world ci cd "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
