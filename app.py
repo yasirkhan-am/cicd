a27b59cffec43e5af75901928645a7715f1da65b
yasirkhan-am <muhammad.yasir@artisticmilliners.com>
2026-07-02 15:52:41 +0500

    file name updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ PORT = int(os.getenv("PORT", 8000))
 
 @app.route("/")
 def home():
-    return "Hello"
+    return "Hello python python app"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
